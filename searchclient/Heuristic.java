0d3912a683898f4c555249e99e41cda4a12ca7a4
TanguyNav <54906232+TanguyNav@users.noreply.github.com>
2020-03-30 15:13:22 +0200

    Add files via upload
    Par rapport à la version de l'Assignement 1,  le principal ajout est l'implémentation d'une nouvelle classe "LevelItems" dans laquelle on définit une fois les emplacements des murs et objectifs pour tout le niveau
    Prochaine tâche: Travail dans SearchClient sur le BufferedReader pour prendre en compte la nouvelle présentation des informations dans les fichiers texte des niveaux

---
+++ searchclient/Heuristic.java
@@ -0,0 +1,71 @@
+package searchclient;
+
+import java.util.Comparator;
+
+public abstract class Heuristic implements Comparator<State> {
+    public Heuristic(State initialState) {
+        // Here's a chance to pre-process the static parts of the level.
+    }
+
+    public int h(State n) {
+        throw new NotImplementedException();
+    }
+
+    public abstract int f(State n);
+
+    @Override
+    public int compare(State n1, State n2) {
+        return this.f(n1) - this.f(n2);
+    }
+
+    public static class AStar extends Heuristic {
+        public AStar(State initialState) {
+            super(initialState);
+        }
+
+        @Override
+        public int f(State n) {
+            return n.g() + this.h(n);
+        }
+
+        @Override
+        public String toString() {
+            return "A* evaluation";
+        }
+    }
+
+    public static class WeightedAStar extends Heuristic {
+        private int W;
+
+        public WeightedAStar(State initialState, int W) {
+            super(initialState);
+            this.W = W;
+        }
+
+        @Override
+        public int f(State n) {
+            return n.g() + this.W * this.h(n);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("WA*(%d) evaluation", this.W);
+        }
+    }
+
+    public static class Greedy extends Heuristic {
+        public Greedy(State initialState) {
+            super(initialState);
+        }
+
+        @Override
+        public int f(State n) {
+            return this.h(n);
+        }
+
+        @Override
+        public String toString() {
+            return "Greedy evaluation";
+        }
+    }
+}
---
+++ searchclient/State.java
@@ -0,0 +1,206 @@
+package searchclient;
+
+import java.util.*;
+
+public class State {
+    private static final Random RNG = new Random(1);
+
+    public static int MAX_ROW = LevelItems.getInstance().getMAX_ROW();
+    public static int MAX_COL = LevelItems.getInstance().getMAX_COL();
+
+    public int agentRow;
+    public int agentCol;
+
+    // Arrays are indexed from the top-left of the level, with first index being row and second being column.
+    // Row 0: (0,0) (0,1) (0,2) (0,3) ...
+    // Row 1: (1,0) (1,1) (1,2) (1,3) ...
+    // Row 2: (2,0) (2,1) (2,2) (2,3) ...
+    // ...
+    // (Start in the top left corner, first go down, then go right)
+    // E.g. this.walls[2] is an array of booleans having size MAX_COL.
+    // this.walls[row][col] is true if there's a wall at (row, col)
+    //
+
+    public static boolean[][] walls = LevelItems.getInstance().getWalls();
+    public static char[][] goals = LevelItems.getInstance().getGoals();
+    public char[][] boxes = new char[MAX_ROW][MAX_COL];
+
+    public State parent;
+    public Command action;
+
+    private int g;
+
+    private int _hash = 0;
+
+    public State(State parent) {
+        this.parent = parent;
+        if (parent == null) {
+            this.g = 0;
+        } else {
+            this.g = parent.g() + 1;
+        }
+    }
+
+    public int g() {
+        return this.g;
+    }
+
+    public boolean isInitialState() {
+        return this.parent == null;
+    }
+
+    public boolean isGoalState() {
+        for (int row = 1; row < MAX_ROW - 1; row++) {
+            for (int col = 1; col < MAX_COL - 1; col++) {
+                char g = goals[row][col];
+                char b = Character.toLowerCase(boxes[row][col]);
+                if (g > 0 && b != g) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<State> getExpandedStates() {
+        ArrayList<State> expandedStates = new ArrayList<>(Command.EVERY.length);
+        for (Command c : Command.EVERY) {
+            // Determine applicability of action
+            int newAgentRow = this.agentRow + Command.dirToRowChange(c.dir1);
+            int newAgentCol = this.agentCol + Command.dirToColChange(c.dir1);
+
+            if (c.actionType == Command.Type.Move) {
+                // Check if there's a wall or box on the cell to which the agent is moving
+                if (this.cellIsFree(newAgentRow, newAgentCol)) {
+                    State n = this.ChildState();
+                    n.action = c;
+                    n.agentRow = newAgentRow;
+                    n.agentCol = newAgentCol;
+                    expandedStates.add(n);
+                }
+            } else if (c.actionType == Command.Type.Push) {
+                // Make sure that there's actually a box to move
+                if (this.boxAt(newAgentRow, newAgentCol)) {
+                    int newBoxRow = newAgentRow + Command.dirToRowChange(c.dir2);
+                    int newBoxCol = newAgentCol + Command.dirToColChange(c.dir2);
+                    // .. and that new cell of box is free
+                    if (this.cellIsFree(newBoxRow, newBoxCol)) {
+                        State n = this.ChildState();
+                        n.action = c;
+                        n.agentRow = newAgentRow;
+                        n.agentCol = newAgentCol;
+                        n.boxes[newBoxRow][newBoxCol] = this.boxes[newAgentRow][newAgentCol];
+                        n.boxes[newAgentRow][newAgentCol] = 0;
+                        expandedStates.add(n);
+                    }
+                }
+            } else if (c.actionType == Command.Type.Pull) {
+                // Cell is free where agent is going
+                if (this.cellIsFree(newAgentRow, newAgentCol)) {
+                    int boxRow = this.agentRow + Command.dirToRowChange(c.dir2);
+                    int boxCol = this.agentCol + Command.dirToColChange(c.dir2);
+                    // .. and there's a box in "dir2" of the agent
+                    if (this.boxAt(boxRow, boxCol)) {
+                        State n = this.ChildState();
+                        n.action = c;
+                        n.agentRow = newAgentRow;
+                        n.agentCol = newAgentCol;
+                        n.boxes[this.agentRow][this.agentCol] = this.boxes[boxRow][boxCol];
+                        n.boxes[boxRow][boxCol] = 0;
+                        expandedStates.add(n);
+                    }
+                }
+            }
+        }
+        Collections.shuffle(expandedStates, RNG);
+        return expandedStates;
+    }
+
+    private boolean cellIsFree(int row, int col) {
+        return !State.walls[row][col] && this.boxes[row][col] == 0;
+    }
+
+    private boolean boxAt(int row, int col) {
+        return this.boxes[row][col] > 0;
+    }
+
+    private State ChildState() {
+        State copy = new State(this);
+        for (int row = 0; row < MAX_ROW; row++) {
+            //System.arraycopy(this.walls[row], 0, copy.walls[row], 0, MAX_COL);
+            System.arraycopy(this.boxes[row], 0, copy.boxes[row], 0, MAX_COL);
+            //System.arraycopy(this.goals[row], 0, copy.goals[row], 0, MAX_COL);
+        }
+        return copy;
+    }
+
+    public ArrayList<State> extractPlan() {
+		ArrayList<State> plan = new ArrayList<>(); // LinkedList<Node> plan = new LinkedList<Node>();
+        State n = this;
+        while (!n.isInitialState()) {
+            plan.add(n);
+            n = n.parent;
+        }
+        Collections.reverse(plan); // No need of this if using a LinkedList
+        return plan;
+    }
+
+    @Override
+    public int hashCode() {
+        if (this._hash == 0) {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + this.agentCol;
+            result = prime * result + this.agentRow;
+            result = prime * result + Arrays.deepHashCode(this.boxes);
+            result = prime * result + Arrays.deepHashCode(State.goals);
+            result = prime * result + Arrays.deepHashCode(State.walls);
+            this._hash = result;
+        }
+        return this._hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        State other = (State) obj;
+        if (this.agentRow != other.agentRow || this.agentCol != other.agentCol)
+            return false;
+        if (!Arrays.deepEquals(this.boxes, other.boxes))
+            return false;
+        if (!Arrays.deepEquals(State.goals, State.goals))
+            return false;
+        return Arrays.deepEquals(State.walls, State.walls);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int row = 0; row < MAX_ROW; row++) {
+            if (!State.walls[row][0]) {
+                break;
+            }
+            for (int col = 0; col < MAX_COL; col++) {
+                if (this.boxes[row][col] > 0) {
+                    s.append(this.boxes[row][col]);
+                } else if (State.goals[row][col] > 0) {
+                    s.append(State.goals[row][col]);
+                } else if (State.walls[row][col]) {
+                    s.append("+");
+                } else if (row == this.agentRow && col == this.agentCol) {
+                    s.append("0");
+                } else {
+                    s.append(" ");
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+}

70be487ae8d8dc66311f7bb882b9c63907b8e1fd
TanNav <tanguynavez@yahoo.fr>
2020-04-17 11:01:16 +0200

    Creation of the class BoardObject and its extensions

---
+++ searchclient/Agent.java
@@ -0,0 +1,20 @@
+package searchclient;
+
+
+public class Agent extends BoardObject {
+
+
+    /**
+	 * Instantiate a new Agent
+	 * 
+	 * @param id the id
+	 * @param color the color
+	 */
+    public Agent(String id, String color) {
+		super(id, color);
+    }
+    
+
+
+
+}

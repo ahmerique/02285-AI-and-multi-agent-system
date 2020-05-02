932731843bebb6c54973db71a70644ef297f2616
ahme <ahmouckerick@gmail.Com>
2020-05-02 18:06:43 +0200

    add Event service

---
+++ src/searchclient/events/EventTemplate.java
@@ -0,0 +1,4 @@
+package searchclient.events;
+
+public class EventTemplate {
+}
---
+++ src/searchclient/services/ThreadExecutorService.java
@@ -0,0 +1,29 @@
+package searchclient.services;
+
+
+import searchclient.AgentThread;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadExecutorService {
+
+    private static ThreadPoolExecutor threadExecutor;
+
+    public static void execute(AgentThread thread) {
+        threadExecutor.execute(thread);
+    }
+
+    public static void shutdown() {
+        threadExecutor.shutdown();
+    }
+
+    public static ThreadPoolExecutor getAgentExecutor() {
+        return threadExecutor;
+    }
+
+    public static void launch(int numberOfAgents) {
+        // (ThreadPoolExecutor) Executors.newFixedThreadPool(numberOfAgents, Thread::new)
+        threadExecutor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+        // Maybe change the keepalivetime
+    }
+}

a56e5982d31a152fbc855c255f6a24cfb352ffca
ahme <ahmouckerick@gmail.Com>
2020-05-20 15:02:54 +0200

    force push for cleaning previous push error

--- src/searchclient/SearchClient.java
+++ src/searchclient/SearchClient.java
@@ -3,7 +3,6 @@ package src.searchclient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
 import java.util.*;
 
 import static java.lang.Character.toLowerCase;
